fca4ed953230cd5eaa8a09b58cb737a85624bf87
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2021-01-03 01:16:59 +0900

    Fix tests

--- __tests__/BunyanAdaptor.test.ts
+++ __tests__/BunyanAdaptor.test.ts
@@ -10,19 +10,225 @@ beforeEach(() => {
 });
 
 const loggerName = 'test-logger';
-const logger = bunyan.createLogger({
-  name: loggerName,
-  level: 'trace',
-  streams: [{ type: 'raw', stream: mockRingBuffer }],
+
+describe('Each logger method', () => {
+  const logger = bunyan.createLogger({
+    name: loggerName,
+    level: 'trace',
+    streams: [{ type: 'raw', stream: mockRingBuffer }],
+  });
+
+  describe('logQuery()', () => {
+    const enabledOptions: LoggerOptions[] = [true, 'all', ['query']];
+
+    test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logQuery('select 1');
+
+      expect(mockRingBuffer.write).toHaveBeenCalledWith({
+        type: 'Query',
+        hostname: expect.any(String),
+        level: bunyan.INFO,
+        msg: 'query: select 1',
+        name: loggerName,
+        pid: expect.any(Number),
+        time: expect.any(Date),
+        v: 0,
+      });
+    });
+
+    test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logQuery('select ?', [1]);
+
+      expect(mockRingBuffer.write).toHaveBeenCalledWith({
+        type: 'Query',
+        hostname: expect.any(String),
+        level: bunyan.INFO,
+        msg: 'query: select ? -- PARAMETERS: [1]',
+        name: loggerName,
+        pid: expect.any(Number),
+        time: expect.any(Date),
+        v: 0,
+      });
+    });
+
+    test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logQuery('select 1');
+
+      expect(mockRingBuffer.write).not.toBeCalled();
+    });
+  });
+
+  describe('logQueryError()', () => {
+    const enabledOptions: LoggerOptions[] = [true, 'all', ['error']];
+
+    test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logQueryError(
+        new Error("Table 'test.Y' doesn't exist"),
+        'select X from Y'
+      );
+
+      expect(mockRingBuffer.write).toHaveBeenCalledWith({
+        type: 'QueryError',
+        hostname: expect.any(String),
+        level: bunyan.ERROR,
+        msg: 'query failed: select X from Y',
+        err: {
+          message: "Table 'test.Y' doesn't exist",
+          name: 'Error',
+          stack: expect.stringMatching(/^Error: Table 'test.Y' doesn't exist\n {4}at .*/m),
+          signal: undefined,
+          code: undefined,
+        },
+        name: loggerName,
+        pid: expect.any(Number),
+        time: expect.any(Date),
+        v: 0,
+      });
+    });
+
+    test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logQueryError(
+        new Error("Table 'test.Y' doesn't exist"),
+        'select ? from Y',
+        [1]
+      );
+
+      expect(mockRingBuffer.write).toHaveBeenCalledWith({
+        type: 'QueryError',
+        hostname: expect.any(String),
+        level: bunyan.ERROR,
+        msg: 'query failed: select ? from Y -- PARAMETERS: [1]',
+        err: {
+          message: "Table 'test.Y' doesn't exist",
+          name: 'Error',
+          stack: expect.stringMatching(/^Error: Table 'test.Y' doesn't exist\n {4}at .*/m),
+          signal: undefined,
+          code: undefined,
+        },
+        name: loggerName,
+        pid: expect.any(Number),
+        time: expect.any(Date),
+        v: 0,
+      });
+    });
+
+    test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logQueryError(
+        new Error("Table 'test.Y' doesn't exist"),
+        'select X from Y'
+      );
+
+      expect(mockRingBuffer.write).not.toBeCalled();
+    });
+  });
+
+  describe('logQuerySlow()', () => {
+    const enabledOptions: LoggerOptions[] = allLoggerOptions;
+
+    test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logQuerySlow(2000, 'select sleep(2)');
+
+      expect(mockRingBuffer.write).toHaveBeenCalledWith({
+        type: 'QuerySlow',
+        hostname: expect.any(String),
+        level: bunyan.WARN,
+        msg: 'query is slow: execution time = 2000, query = select sleep(2)',
+        name: loggerName,
+        pid: expect.any(Number),
+        time: expect.any(Date),
+        executionTime: 2000,
+        v: 0,
+      });
+    });
+
+    test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logQuerySlow(2000, 'select sleep(?)', [2]);
+
+      expect(mockRingBuffer.write).toHaveBeenCalledWith({
+        type: 'QuerySlow',
+        hostname: expect.any(String),
+        level: bunyan.WARN,
+        msg: 'query is slow: execution time = 2000, query = select sleep(?) -- PARAMETERS: [2]',
+        name: loggerName,
+        pid: expect.any(Number),
+        time: expect.any(Date),
+        executionTime: 2000,
+        v: 0,
+      });
+    });
+  });
+
+  describe('logSchemaBuild()', () => {
+    const enabledOptions: LoggerOptions[] = [true, 'all', ['schema']];
+
+    test.each<LoggerOptions>(enabledOptions)('LoggerOptions = %s', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logSchemaBuild('creating a new table: memo');
+
+      expect(mockRingBuffer.write).toHaveBeenCalledWith({
+        type: 'SchemaBuild',
+        hostname: expect.any(String),
+        level: bunyan.DEBUG,
+        msg: 'creating a new table: memo',
+        name: loggerName,
+        pid: expect.any(Number),
+        time: expect.any(Date),
+        v: 0,
+      });
+    });
+
+    test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logSchemaBuild('creating a new table: memo');
+
+      expect(mockRingBuffer.write).not.toBeCalled();
+    });
+  });
+
+  describe('logMigration()', () => {
+    const enabledOptions: LoggerOptions[] = [true, 'all', ['migration']];
+
+    test.each<LoggerOptions>(enabledOptions)('LoggerOptions = %s', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logMigration('(migration message)');
+
+      expect(mockRingBuffer.write).toHaveBeenCalledWith({
+        type: 'Migration',
+        hostname: expect.any(String),
+        level: bunyan.DEBUG,
+        msg: '(migration message)',
+        name: loggerName,
+        pid: expect.any(Number),
+        time: expect.any(Date),
+        v: 0,
+      });
+    });
+
+    test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
+      new BunyanAdaptor(logger, loggerOptions).logMigration('(migration message)');
+
+      expect(mockRingBuffer.write).not.toBeCalled();
+    });
+  });
 });
 
-describe('logQuery()', () => {
-  const enabledOptions: LoggerOptions[] = [true, 'all', ['query']];
+describe('Customized logger methods', () => {
+  const logger = bunyan.createLogger({
+    name: loggerName,
+    level: 'info',
+    streams: [{ type: 'raw', stream: mockRingBuffer }],
+  });
 
-  test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logQuery('select 1');
+  function invokeLoggerMethods(adaptor: BunyanAdaptor): void {
+    adaptor.logQuery('select 1');
+    adaptor.logQueryError(new Error('Error message'), 'select X from Y');
+    adaptor.logQuerySlow(2000, 'select sleep(2)');
+    adaptor.logSchemaBuild('creating a new table: memo');
+    adaptor.logMigration('(migration message)');
+  }
 
-    expect(mockRingBuffer.write).toHaveBeenCalledWith({
+  test('Default log level methods', () => {
+    invokeLoggerMethods(new BunyanAdaptor(logger, 'all'));
+
+    expect(mockRingBuffer.write).toBeCalledTimes(3);
+    expect(mockRingBuffer.write).toHaveBeenNthCalledWith(1, {
       type: 'Query',
       hostname: expect.any(String),
       level: bunyan.INFO,
@@ -32,101 +238,18 @@ describe('logQuery()', () => {
       time: expect.any(Date),
       v: 0,
     });
-  });
-
-  test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logQuery('select ?', [1]);
-
-    expect(mockRingBuffer.write).toHaveBeenCalledWith({
-      type: 'Query',
-      hostname: expect.any(String),
-      level: bunyan.INFO,
-      msg: 'query: select ? -- PARAMETERS: [1]',
-      name: loggerName,
-      pid: expect.any(Number),
-      time: expect.any(Date),
-      v: 0,
-    });
-  });
-
-  test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logQuery('select 1');
-
-    expect(mockRingBuffer.write).not.toBeCalled();
-  });
-});
-
-describe('logQueryError()', () => {
-  const enabledOptions: LoggerOptions[] = [true, 'all', ['error']];
-
-  test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logQueryError(
-      new Error("Table 'test.Y' doesn't exist"),
-      'select X from Y'
-    );
-
-    expect(mockRingBuffer.write).toHaveBeenCalledWith({
+    expect(mockRingBuffer.write).toHaveBeenNthCalledWith(2, {
       type: 'QueryError',
       hostname: expect.any(String),
       level: bunyan.ERROR,
       msg: 'query failed: select X from Y',
-      err: {
-        message: "Table 'test.Y' doesn't exist",
-        name: 'Error',
-        stack: expect.stringMatching(/^Error: Table 'test.Y' doesn't exist\n {4}at .*/m),
-        signal: undefined,
-        code: undefined,
-      },
+      err: expect.any(Object),
       name: loggerName,
       pid: expect.any(Number),
       time: expect.any(Date),
       v: 0,
     });
-  });
-
-  test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logQueryError(
-      new Error("Table 'test.Y' doesn't exist"),
-      'select ? from Y',
-      [1]
-    );
-
-    expect(mockRingBuffer.write).toHaveBeenCalledWith({
-      type: 'QueryError',
-      hostname: expect.any(String),
-      level: bunyan.ERROR,
-      msg: 'query failed: select ? from Y -- PARAMETERS: [1]',
-      err: {
-        message: "Table 'test.Y' doesn't exist",
-        name: 'Error',
-        stack: expect.stringMatching(/^Error: Table 'test.Y' doesn't exist\n {4}at .*/m),
-        signal: undefined,
-        code: undefined,
-      },
-      name: loggerName,
-      pid: expect.any(Number),
-      time: expect.any(Date),
-      v: 0,
-    });
-  });
-
-  test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logQueryError(
-      new Error("Table 'test.Y' doesn't exist"),
-      'select X from Y'
-    );
-
-    expect(mockRingBuffer.write).not.toBeCalled();
-  });
-});
-
-describe('logQuerySlow()', () => {
-  const enabledOptions: LoggerOptions[] = allLoggerOptions;
-
-  test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logQuerySlow(2000, 'select sleep(2)');
-
-    expect(mockRingBuffer.write).toHaveBeenCalledWith({
+    expect(mockRingBuffer.write).toHaveBeenNthCalledWith(3, {
       type: 'QuerySlow',
       hostname: expect.any(String),
       level: bunyan.WARN,
@@ -139,69 +262,83 @@ describe('logQuerySlow()', () => {
     });
   });
 
-  test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logQuerySlow(2000, 'select sleep(?)', [2]);
+  test('DEBUG log level methods', () => {
+    invokeLoggerMethods(
+      new BunyanAdaptor(logger, 'all', {
+        log: 'debug',
+        info: 'debug',
+        warn: 'debug',
+        error: 'debug',
+      })
+    );
 
-    expect(mockRingBuffer.write).toHaveBeenCalledWith({
+    expect(mockRingBuffer.write).not.toBeCalled();
+  });
+
+  test('INFO log level methods', () => {
+    invokeLoggerMethods(
+      new BunyanAdaptor(logger, 'all', {
+        log: 'info',
+        info: 'info',
+        warn: 'info',
+        error: 'info',
+        query: 'info',
+        queryError: 'info',
+      })
+    );
+
+    expect(mockRingBuffer.write).toBeCalledTimes(5);
+    expect(mockRingBuffer.write).toHaveBeenNthCalledWith(1, {
+      type: 'Query',
+      hostname: expect.any(String),
+      level: bunyan.INFO,
+      msg: 'query: select 1',
+      name: loggerName,
+      pid: expect.any(Number),
+      time: expect.any(Date),
+      v: 0,
+    });
+    expect(mockRingBuffer.write).toHaveBeenNthCalledWith(2, {
+      type: 'QueryError',
+      hostname: expect.any(String),
+      level: bunyan.INFO,
+      msg: 'query failed: select X from Y',
+      err: expect.any(Object),
+      name: loggerName,
+      pid: expect.any(Number),
+      time: expect.any(Date),
+      v: 0,
+    });
+    expect(mockRingBuffer.write).toHaveBeenNthCalledWith(3, {
       type: 'QuerySlow',
       hostname: expect.any(String),
-      level: bunyan.WARN,
-      msg: 'query is slow: execution time = 2000, query = select sleep(?) -- PARAMETERS: [2]',
+      level: bunyan.INFO,
+      msg: 'query is slow: execution time = 2000, query = select sleep(2)',
       name: loggerName,
       pid: expect.any(Number),
       time: expect.any(Date),
       executionTime: 2000,
       v: 0,
     });
-  });
-});
-
-describe('logSchemaBuild()', () => {
-  const enabledOptions: LoggerOptions[] = [true, 'all', ['schema']];
-
-  test.each<LoggerOptions>(enabledOptions)('LoggerOptions = %s', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logSchemaBuild('creating a new table: memo');
-
-    expect(mockRingBuffer.write).toHaveBeenCalledWith({
+    expect(mockRingBuffer.write).toHaveBeenNthCalledWith(4, {
       type: 'SchemaBuild',
       hostname: expect.any(String),
-      level: bunyan.DEBUG,
+      level: bunyan.INFO,
       msg: 'creating a new table: memo',
       name: loggerName,
       pid: expect.any(Number),
       time: expect.any(Date),
       v: 0,
     });
-  });
-
-  test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logSchemaBuild('creating a new table: memo');
-
-    expect(mockRingBuffer.write).not.toBeCalled();
-  });
-});
-
-describe('logMigration()', () => {
-  const enabledOptions: LoggerOptions[] = [true, 'all', ['migration']];
-
-  test.each<LoggerOptions>(enabledOptions)('LoggerOptions = %s', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logMigration('(migration message)');
-
-    expect(mockRingBuffer.write).toHaveBeenCalledWith({
+    expect(mockRingBuffer.write).toHaveBeenNthCalledWith(5, {
       type: 'Migration',
       hostname: expect.any(String),
-      level: bunyan.DEBUG,
+      level: bunyan.INFO,
       msg: '(migration message)',
       name: loggerName,
       pid: expect.any(Number),
       time: expect.any(Date),
       v: 0,
     });
-  });
-
-  test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
-    new BunyanAdaptor(logger, loggerOptions).logMigration('(migration message)');
-
-    expect(mockRingBuffer.write).not.toBeCalled();
   });
 });
--- __tests__/WinstonAdaptor.test.ts
+++ __tests__/WinstonAdaptor.test.ts
@@ -9,114 +9,180 @@ beforeEach(() => {
   mockReset(mockStream);
 });
 
-const logger = winston.createLogger({
-  level: 'debug',
-  format: winston.format.simple(),
-  transports: [new winston.transports.Stream({ stream: mockStream })],
+describe('Each logger method', () => {
+  const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.simple(),
+    transports: [new winston.transports.Stream({ stream: mockStream })],
+  });
+
+  describe('logQuery()', () => {
+    const enabledOptions: LoggerOptions[] = [true, 'all', ['query']];
+
+    test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logQuery('select 1');
+
+      expect(mockStream.write).toHaveBeenCalledWith('info: query: select 1\n');
+    });
+
+    test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logQuery('select ?', [1]);
+
+      expect(mockStream.write).toHaveBeenCalledWith('info: query: select ? -- PARAMETERS: [1]\n');
+    });
+
+    test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logQuery('select 1');
+
+      expect(mockStream.write).not.toBeCalled();
+    });
+  });
+
+  describe('logQueryError()', () => {
+    const enabledOptions: LoggerOptions[] = [true, 'all', ['error']];
+
+    test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logQueryError(
+        new Error("Table 'test.Y' doesn't exist"),
+        'select X from Y'
+      );
+
+      expect(mockStream.write).toHaveBeenCalledWith('error: query failed: select X from Y\n');
+    });
+
+    test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logQueryError(
+        new Error("Table 'test.Y' doesn't exist"),
+        'select ? from Y',
+        [1]
+      );
+
+      expect(mockStream.write).toHaveBeenCalledWith('error: query failed: select ? from Y -- PARAMETERS: [1]\n');
+    });
+
+    test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logQueryError(
+        new Error("Table 'test.Y' doesn't exist"),
+        'select X from Y'
+      );
+
+      expect(mockStream.write).not.toBeCalled();
+    });
+  });
+
+  describe('logQuerySlow()', () => {
+    const enabledOptions: LoggerOptions[] = allLoggerOptions;
+
+    test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logQuerySlow(2000, 'select sleep(2)');
+
+      expect(mockStream.write).toHaveBeenCalledWith(
+        'warn: query is slow: execution time = 2000, query = select sleep(2)\n'
+      );
+    });
+
+    test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logQuerySlow(2000, 'select sleep(?)', [2]);
+
+      expect(mockStream.write).toHaveBeenCalledWith(
+        'warn: query is slow: execution time = 2000, query = select sleep(?) -- PARAMETERS: [2]\n'
+      );
+    });
+  });
+
+  describe('logSchemaBuild()', () => {
+    const enabledOptions: LoggerOptions[] = [true, 'all', ['schema']];
+
+    test.each<LoggerOptions>(enabledOptions)('LoggerOptions = %s', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logSchemaBuild('creating a new table: memo');
+
+      expect(mockStream.write).toHaveBeenCalledWith('debug: creating a new table: memo\n');
+    });
+
+    test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logSchemaBuild('creating a new table: memo');
+
+      expect(mockStream.write).not.toBeCalled();
+    });
+  });
+
+  describe('logMigration()', () => {
+    const enabledOptions: LoggerOptions[] = [true, 'all', ['migration']];
+
+    test.each<LoggerOptions>(enabledOptions)('LoggerOptions = %s', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logMigration('(migration message)');
+
+      expect(mockStream.write).toHaveBeenCalledWith('debug: (migration message)\n');
+    });
+
+    test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
+      new WinstonAdaptor(logger, loggerOptions).logMigration('(migration message)');
+
+      expect(mockStream.write).not.toBeCalled();
+    });
+  });
 });
 
-describe('logQuery()', () => {
-  const enabledOptions: LoggerOptions[] = [true, 'all', ['query']];
-
-  test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logQuery('select 1');
-
-    expect(mockStream.write).toHaveBeenCalledWith('info: query: select 1\n');
+describe('Customized logger methods', () => {
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.simple(),
+    transports: [new winston.transports.Stream({ stream: mockStream })],
   });
 
-  test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logQuery('select ?', [1]);
+  function invokeLoggerMethods(adaptor: WinstonAdaptor): void {
+    adaptor.logQuery('select 1');
+    adaptor.logQueryError(new Error('Error message'), 'select X from Y');
+    adaptor.logQuerySlow(2000, 'select sleep(2)');
+    adaptor.logSchemaBuild('creating a new table: memo');
+    adaptor.logMigration('(migration message)');
+  }
 
-    expect(mockStream.write).toHaveBeenCalledWith('info: query: select ? -- PARAMETERS: [1]\n');
-  });
+  test('Default log level methods', () => {
+    invokeLoggerMethods(new WinstonAdaptor(logger, 'all'));
 
-  test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logQuery('select 1');
-
-    expect(mockStream.write).not.toBeCalled();
-  });
-});
-
-describe('logQueryError()', () => {
-  const enabledOptions: LoggerOptions[] = [true, 'all', ['error']];
-
-  test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logQueryError(
-      new Error("Table 'test.Y' doesn't exist"),
-      'select X from Y'
-    );
-
-    expect(mockStream.write).toHaveBeenCalledWith('error: query failed: select X from Y\n');
-  });
-
-  test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logQueryError(
-      new Error("Table 'test.Y' doesn't exist"),
-      'select ? from Y',
-      [1]
-    );
-
-    expect(mockStream.write).toHaveBeenCalledWith('error: query failed: select ? from Y -- PARAMETERS: [1]\n');
-  });
-
-  test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logQueryError(
-      new Error("Table 'test.Y' doesn't exist"),
-      'select X from Y'
-    );
-
-    expect(mockStream.write).not.toBeCalled();
-  });
-});
-
-describe('logQuerySlow()', () => {
-  const enabledOptions: LoggerOptions[] = allLoggerOptions;
-
-  test.each<LoggerOptions>(enabledOptions)('w/o parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logQuerySlow(2000, 'select sleep(2)');
-
-    expect(mockStream.write).toHaveBeenCalledWith(
+    expect(mockStream.write).toBeCalledTimes(3);
+    expect(mockStream.write).toHaveBeenNthCalledWith(1, 'info: query: select 1\n');
+    expect(mockStream.write).toHaveBeenNthCalledWith(2, 'error: query failed: select X from Y\n');
+    expect(mockStream.write).toHaveBeenNthCalledWith(
+      3,
       'warn: query is slow: execution time = 2000, query = select sleep(2)\n'
     );
   });
 
-  test.each<LoggerOptions>(enabledOptions)('with parameters (LoggerOptions = %s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logQuerySlow(2000, 'select sleep(?)', [2]);
-
-    expect(mockStream.write).toHaveBeenCalledWith(
-      'warn: query is slow: execution time = 2000, query = select sleep(?) -- PARAMETERS: [2]\n'
+  test('DEBUG log level methods', () => {
+    invokeLoggerMethods(
+      new WinstonAdaptor(logger, 'all', false, {
+        log: logger.debug,
+        info: logger.debug,
+        warn: logger.debug,
+        error: logger.debug,
+      })
     );
-  });
-});
-
-describe('logSchemaBuild()', () => {
-  const enabledOptions: LoggerOptions[] = [true, 'all', ['schema']];
-
-  test.each<LoggerOptions>(enabledOptions)('LoggerOptions = %s', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logSchemaBuild('creating a new table: memo');
-
-    expect(mockStream.write).toHaveBeenCalledWith('debug: creating a new table: memo\n');
-  });
-
-  test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logSchemaBuild('creating a new table: memo');
 
     expect(mockStream.write).not.toBeCalled();
   });
-});
 
-describe('logMigration()', () => {
-  const enabledOptions: LoggerOptions[] = [true, 'all', ['migration']];
+  test('INFO log level methods', () => {
+    invokeLoggerMethods(
+      new WinstonAdaptor(logger, 'all', false, {
+        log: logger.info,
+        info: logger.info,
+        warn: logger.info,
+        error: logger.info,
+        query: logger.info,
+        queryError: logger.info,
+      })
+    );
 
-  test.each<LoggerOptions>(enabledOptions)('LoggerOptions = %s', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logMigration('(migration message)');
-
-    expect(mockStream.write).toHaveBeenCalledWith('debug: (migration message)\n');
-  });
-
-  test.each<LoggerOptions>(otherLoggerOptions(enabledOptions))('other LoggerOptions (%s)', (loggerOptions) => {
-    new WinstonAdaptor(logger, loggerOptions).logMigration('(migration message)');
-
-    expect(mockStream.write).not.toBeCalled();
+    expect(mockStream.write).toBeCalledTimes(5);
+    expect(mockStream.write).toHaveBeenNthCalledWith(1, 'info: query: select 1\n');
+    expect(mockStream.write).toHaveBeenNthCalledWith(2, 'info: query failed: select X from Y\n');
+    expect(mockStream.write).toHaveBeenNthCalledWith(
+      3,
+      'info: query is slow: execution time = 2000, query = select sleep(2)\n'
+    );
+    expect(mockStream.write).toHaveBeenNthCalledWith(4, 'info: creating a new table: memo\n');
+    expect(mockStream.write).toHaveBeenNthCalledWith(5, 'info: (migration message)\n');
   });
 });
