df3011e03bde1819e5e363a4857a554a89fa84a1
KOMIYA Atsushi <komiya.atsushi@gmail.com>
2021-01-07 22:04:28 +0900

    Use jest-junit to report test result

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,22 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: [ "**/__tests__/**/*.test.ts" ],
+  testMatch: ['**/__tests__/**/*.test.ts'],
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
-    }
+      tsconfig: 'tsconfig.test.json',
+    },
   },
   testTimeout: 60000,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        suiteNameTemplate: '{filepath}',
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+      },
+    ],
+  ],
 };
